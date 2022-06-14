555451615b518d575286544f6a7f9259b168a76c
Axtane <axtane3456@gmail.com>
2022-06-14 18:50:31 +0300

    fixed encoding issue for utf-8 and get a practice with select\option tags

---
+++ src/main/java/ru/axtane/springMVC/controllers/AdminController.java
@@ -0,0 +1,34 @@
+package ru.axtane.springMVC.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.axtane.springMVC.dao.PersonDAO;
+import ru.axtane.springMVC.models.Person;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String adminPage(Model model, @ModelAttribute("person") Person person){
+        model.addAttribute("people", personDAO.index());
+        return "adminPage";
+    }
+
+    @PatchMapping("/add")
+    public String maleAdmin(@ModelAttribute("person") Person person){
+        System.out.println(person.getId());
+        return "redirect:/people";
+    }
+}
