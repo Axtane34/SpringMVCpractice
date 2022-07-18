971a465cf4efc0a2418d97b741616d6d69d86a28
Axtane <axtane3456@gmail.com>
2022-07-18 14:48:33 +0300

    delete things which have not any sense in this project

--- src/main/java/ru/axtane/springMVC/controllers/ComposerController.java
+++ src/main/java/ru/axtane/springMVC/controllers/ComposerController.java
@@ -6,18 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.axtane.springMVC.dao.ComposerDAO;
 import ru.axtane.springMVC.models.Composer;
-import ru.axtane.springMVC.util.ComposerValidator;
+
 
 @Controller
 @RequestMapping("/composer")
 public class ComposerController {
     private final ComposerDAO composerDAO;
-    private final ComposerValidator composerValidator;
+
 
     @Autowired
-    public ComposerController(ComposerDAO composerDAO, ComposerValidator composerValidator) {
+    public ComposerController(ComposerDAO composerDAO) {
         this.composerDAO = composerDAO;
-        this.composerValidator = composerValidator;
     }
 
     @GetMapping("/{name}")
